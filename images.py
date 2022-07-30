314397245bada72bd0ceff42352b91fe230ec176
Alireza Karami <salirezakarami@gmail.com>
2022-07-30 20:22:40 +0430

    write merge func

--- images.py
+++ images.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageDraw, ImageFilter
 def background(image_wh: tuple, background_color: str) -> Image:
     return Image.new('RGBA', image_wh, background_color)
 
+
 def center_xy(main_wh: tuple, sub_wh: tuple) -> tuple:
     return (
        (main_wh[0]-sub_wh[0])/2, #x0
@@ -12,6 +13,7 @@ def center_xy(main_wh: tuple, sub_wh: tuple) -> tuple:
        (main_wh[0]-sub_wh[0])/2+sub_wh[0], #x1
        (main_wh[1]-sub_wh[1])/2+sub_wh[1], #y1
     )
+
 
 def text_box(story_wh: tuple, box_wh: tuple, box_color: str, box_radius: int, box_shadow_radius: int, box_shadow_color: str) -> Image:
     box_xy = center_xy(story_wh, box_wh)
@@ -23,3 +25,8 @@ def text_box(story_wh: tuple, box_wh: tuple, box_color: str, box_radius: int, bo
     ImageDraw.Draw(box_image).rounded_rectangle(box_xy, box_radius, box_color)
 
     return box_image
+
+
+def merge(image_1: Image, image_2: Image) -> Image:
+    image_1.alpha_composite(image_2)
+    return image_1
