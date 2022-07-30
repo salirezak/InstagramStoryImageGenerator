05ad200d75caeafe61ad92f5d8c7058d52649fae
Alireza Karami <salirezakarami@gmail.com>
2022-07-30 21:04:52 +0430

    write generate_background func

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from images import background
+
+
+
 class StoryImageGenerator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, story_wh: tuple, story_color: str) -> None:
+
+        self.story_wh = story_wh
+        self.story_color = story_color
+
+        self.background = self.generate_background()
+
+    def generate_background(self):
+        return background(self.story_wh, self.story_color)
