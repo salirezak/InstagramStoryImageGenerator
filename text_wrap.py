eb35fed4bd17b98fb7a91ba219db147675bc82b8
Alireza Karami <salirezakarami@gmail.com>
2022-07-30 19:58:05 +0430

    use Font class

--- text_wrap.py
+++ text_wrap.py
@@ -1,16 +1,16 @@
-from PIL import ImageFont
+from font import Font
 
 
 
-def line_wrap(line: str, max_width: int, font: ImageFont) -> str:
+def line_wrap(line: str, max_width: int, font: Font) -> str:
     words = line.strip().split()
     lines, line = [], ''
 
     while words:
         word = words.pop(0)
-        if font.getsize(line + ' ' + word)[0] > max_width:
+        if font.get_size(line + ' ' + word)[0] > max_width:
             # break line and start new line
-            if font.getsize(line)[0] > max_width:
+            if font.get_size(line)[0] > max_width:
                 lines.append(line[:max_width].strip())
                 line = line[max_width:].strip()
             # start new line
@@ -25,7 +25,7 @@ def line_wrap(line: str, max_width: int, font: ImageFont) -> str:
     # add last line
     while line:
         # break line and start new line
-        if font.getsize(line)[0] > max_width:
+        if font.get_size(line)[0] > max_width:
                 lines.append(line[:max_width].strip())
                 line = line[max_width:].strip()
         # add the true last line
@@ -36,12 +36,11 @@ def line_wrap(line: str, max_width: int, font: ImageFont) -> str:
     return '\n'.join(lines)
 
 
-def text_wrap(text: str, max_wh: int) -> str:
-    font = ImageFont.truetype('fonts/Vazir.ttf', 10, encoding='unic')
+def text_wrap(text: str, max_wh: tuple, font: Font) -> str:
     lines = text.strip().split('\n')
     lines = map(lambda line: line_wrap(line, max_wh[0], font), lines)
     text = '\n'.join(lines)
 
-    if font.getsize_multiline(text, 'rtl', 10, 'fa')[1] <= max_wh[1]:
+    if font.get_size_multiline(text)[1] <= max_wh[1]:
         return text
     raise Exception('Text is too long')
