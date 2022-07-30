567aa3cc1a244719ad66c4184a1664c06f4adb1e
Alireza Karami <salirezakarami@gmail.com>
2022-07-30 22:55:48 +0430

    write generate_text_box func

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from images import background
+from images import background, text_box
 from font import Font
 from text_wrap import text_wrap
 
 
 
 class StoryImageGenerator:
-    def __init__(self, story_wh: tuple, story_color: str, font_name: str, font_size: int, lines_spacing: int, text_align: str, text: str, box_margin_xy: tuple, text_margin_xy: tuple,
+    def __init__(self,
+    story_wh: tuple, story_color: str,
+    font_name: str, font_size: int, lines_spacing: int, text_align: str, text: str,
+    box_margin_xy: tuple, text_margin_xy: tuple,
+    box_color: str, box_radius: int, box_shadow_radius: int, box_shadow_color: int,
     ) -> None:
 
         self.story_wh = story_wh
@@ -17,9 +21,14 @@ class StoryImageGenerator:
         self.text = text
         self.box_margin_xy = box_margin_xy
         self.text_margin_xy = text_margin_xy
+        self.box_color = box_color
+        self.box_radius = box_radius
+        self.box_shadow_radius = box_shadow_radius
+        self.box_shadow_color = box_shadow_color
 
         self.font = self.get_font()
         self.background = self.generate_background()
+        self.text_box = self.generate_text_box()
 
     def get_font(self) -> Font:
         return Font(self.font_name, self.font_size, spacing=self.lines_spacing, align=self.text_align)
@@ -37,11 +46,21 @@ class StoryImageGenerator:
             max_box_wh[1]-2*self.text_margin_xy[1],
         )
 
+    def text_box_wh(self) -> tuple:
+        text_wh = self.font.get_size_multiline(self.wrapped_text())
+        return (
+            text_wh[0]+2*self.text_margin_xy[0],
+            text_wh[1]+2*self.text_margin_xy[1],
+        )
+
     def wrapped_text(self) -> str:
         return text_wrap(self.text, self.max_text_wh(), self.font)
 
     def generate_background(self):
         return background(self.story_wh, self.story_color)
+
+    def generate_text_box(self):
+        return text_box(self.story_wh, self.text_box_wh(), self.box_color, self.box_radius, self.box_shadow_radius, self.box_shadow_color)
 
     def show(self) -> None:
         self.background.show()
